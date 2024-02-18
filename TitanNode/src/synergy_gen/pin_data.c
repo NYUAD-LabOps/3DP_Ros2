b3ae22c38da7ea6b68f364224371805594f0fc35
NYUAD-LabOps <michael@davisfiles.net>
2024-02-18 04:19:13 -0800

    update

--- TitanNode/src/synergy_gen/pin_data.c
+++ TitanNode/src/synergy_gen/pin_data.c
@@ -43,20 +43,8 @@ const ioport_pin_cfg_t g_bsp_pin_cfg_data[] = {
         .pin_cfg = ((uint32_t) IOPORT_CFG_ANALOG_ENABLE),
     },
     {
-        .pin = IOPORT_PORT_01_PIN_00,
-        .pin_cfg = ((uint32_t) IOPORT_CFG_DRIVE_MID | (uint32_t) IOPORT_CFG_PERIPHERAL_PIN | (uint32_t) IOPORT_PERIPHERAL_RSPI),
-    },
-    {
-        .pin = IOPORT_PORT_01_PIN_01,
-        .pin_cfg = ((uint32_t) IOPORT_CFG_DRIVE_MID | (uint32_t) IOPORT_CFG_PERIPHERAL_PIN | (uint32_t) IOPORT_PERIPHERAL_RSPI),
-    },
-    {
         .pin = IOPORT_PORT_01_PIN_02,
-        .pin_cfg = ((uint32_t) IOPORT_CFG_DRIVE_MID | (uint32_t) IOPORT_CFG_PERIPHERAL_PIN | (uint32_t) IOPORT_PERIPHERAL_RSPI),
-    },
-    {
-        .pin = IOPORT_PORT_01_PIN_03,
-        .pin_cfg = ((uint32_t) IOPORT_CFG_DRIVE_MID | (uint32_t) IOPORT_CFG_PERIPHERAL_PIN | (uint32_t) IOPORT_PERIPHERAL_RSPI),
+        .pin_cfg = ((uint32_t) IOPORT_CFG_PORT_DIRECTION_OUTPUT | (uint32_t) IOPORT_CFG_PORT_OUTPUT_LOW),
     },
     {
         .pin = IOPORT_PORT_01_PIN_04,
