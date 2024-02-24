015bcb0a1fc1a5f4f6d536849f125312c06bacdf
NYUAD-LabOps <michael@davisfiles.net>
2024-02-23 20:49:20 -0800

    Update

--- TitanNode/src/synergy_gen/pin_data.c
+++ TitanNode/src/synergy_gen/pin_data.c
@@ -59,6 +59,14 @@ const ioport_pin_cfg_t g_bsp_pin_cfg_data[] = {
         .pin_cfg = ((uint32_t) IOPORT_CFG_PORT_DIRECTION_OUTPUT | (uint32_t) IOPORT_CFG_PORT_OUTPUT_LOW),
     },
     {
+        .pin = IOPORT_PORT_01_PIN_06,
+        .pin_cfg = ((uint32_t) IOPORT_CFG_PORT_DIRECTION_OUTPUT | (uint32_t) IOPORT_CFG_PORT_OUTPUT_LOW),
+    },
+    {
+        .pin = IOPORT_PORT_01_PIN_07,
+        .pin_cfg = ((uint32_t) IOPORT_CFG_PORT_DIRECTION_OUTPUT | (uint32_t) IOPORT_CFG_PORT_OUTPUT_LOW),
+    },
+    {
         .pin = IOPORT_PORT_01_PIN_08,
         .pin_cfg = ((uint32_t) IOPORT_CFG_PERIPHERAL_PIN | (uint32_t) IOPORT_PERIPHERAL_DEBUG),
     },
@@ -185,6 +193,10 @@ const ioport_pin_cfg_t g_bsp_pin_cfg_data[] = {
     {
         .pin = IOPORT_PORT_05_PIN_07,
         .pin_cfg = ((uint32_t) IOPORT_CFG_PORT_DIRECTION_INPUT),
+    },
+    {
+        .pin = IOPORT_PORT_06_PIN_00,
+        .pin_cfg = ((uint32_t) IOPORT_CFG_PORT_DIRECTION_OUTPUT | (uint32_t) IOPORT_CFG_PORT_OUTPUT_LOW),
     },
     {
         .pin = IOPORT_PORT_06_PIN_01,
